79662a839b6c062197903adf4087c34b1d78d4f7
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-02-27 12:07:06 +0100

    Converted Bootstrap panels to cards.

--- templates/product-details.php
+++ templates/product-details.php
@@ -1,14 +1,15 @@
-<div class="panel">
-	<header>
-		<h3><?php esc_html_e( 'Product Details', 'orbis_products' ); ?></h3>
-	</header>
+<div class="card">
+	<div class="card-body">
+		<h3 class="card-title"><?php esc_html_e( 'Product Details', 'orbis_products' ); ?></h3>
 
-	<div class="content">
-		<dl>
-			<dt><?php esc_html_e( 'Price', 'orbis_products' ); ?></dt>
-			<dd>
-				<?php orbis_product_the_price(); ?>
-			</dd>
-		</dl>
+		<div class="content">
+			<dl>
+				<dt><?php esc_html_e( 'Price', 'orbis_products' ); ?></dt>
+				<dd>
+					<?php orbis_product_the_price(); ?>
+				</dd>
+			</dl>
+		</div>
 	</div>
+	
 </div>
